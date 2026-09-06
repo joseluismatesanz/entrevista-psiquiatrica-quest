9318c5142e47565734385138bb8c1f98333da630
joseluismatesanz <jlmatesanzperez@gmail.com>
2026-09-06 15:14:34 +0200

    Cache Safari iPhone launcher and direct app fragments

--- sw.js
+++ sw.js
@@ -1,24 +1,11 @@
-const CACHE='salud-mental-v038';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
-self.addEventListener('install',event=>{
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
-});
-self.addEventListener('activate',event=>{
-  event.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
-      .then(()=>self.clients.claim())
-  );
-});
+const CACHE='salud-mental-v039-iphone';
+const ASSETS=['./iphone.html','./manifest.webmanifest','./icon.svg','./iphone-parts/part01.txt','./iphone-parts/part02.txt','./iphone-parts/part03.txt','./iphone-parts/part04.txt','./iphone-parts/part05.txt','./iphone-parts/part06.txt','./iphone-parts/part07.txt'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{
-  if(event.request.method!=='GET') return;
-  event.respondWith(
-    caches.match(event.request).then(cached=>{
-      if(cached) return cached;
-      return fetch(event.request).then(response=>{
-        const clone=response.clone();
-        caches.open(CACHE).then(cache=>cache.put(event.request,clone));
-        return response;
-      }).catch(()=>caches.match('./index.html'));
-    })
-  );
+ if(event.request.method!=='GET')return;
+ if(event.request.mode==='navigate'){
+  event.respondWith(fetch(event.request).catch(()=>caches.match('./iphone.html')));return;
+ }
+ event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request)));
 });
